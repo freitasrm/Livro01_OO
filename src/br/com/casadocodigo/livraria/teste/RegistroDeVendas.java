fd02f6e63f5014632c88916b944bbde67b7536ac
RMartins <freitas.rodrigom@gmail.com>
2019-05-03 01:50:02 -0300

    {lviro oo} pagina 153 - Checked Exceptions

--- src/br/com/casadocodigo/livraria/teste/RegistroDeVendas.java
+++ src/br/com/casadocodigo/livraria/teste/RegistroDeVendas.java
@@ -4,6 +4,7 @@ import br.com.casadocodigo.livraria.Autor;
 import br.com.casadocodigo.livraria.produtos.CarrinhoDeCompras;
 import br.com.casadocodigo.livraria.produtos.Ebook;
 import br.com.casadocodigo.livraria.produtos.LivroFisico;
+import br.com.casadocodigo.livraria.produtos.Produto;
 
 public class RegistroDeVendas {
 	
@@ -26,6 +27,30 @@ public class RegistroDeVendas {
 		carrinho.adiciona(ebook);
 		
 		System.out.println("Total: " + carrinho.getTotal());
+		
+		
+		Produto[] produtos = carrinho.getProdutos();
+
+		for(Produto produto : produtos) {
+			if(produto != null) {
+				System.out.println(produto.getValor());
+			}
+		}
+		
+		for(int i = 0; i <= produtos.length; i++) {
+			try {
+				Produto produto = produtos[i];
+				if(produto != null) {
+					System.out.println(produto.getValor());
+				}
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("deu excpetion no indice: " + i);
+			} catch (NullPointerException e) {
+				System.out.println("A array não foi estanciado!");
+			}
+		}
+		
+		System.out.println("Fui executado!");
 	}
 
 }
